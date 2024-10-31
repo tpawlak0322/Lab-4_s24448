574e3015e0f55f427248cf440dd399c448479980
tpawlak0322 <t.pawlak2002@gmail.com>
2024-10-31 23:28:33 +0100

    created main.py to automate process

---
+++ main.py
@@ -0,0 +1,11 @@
+from scripts import load_data as loader
+from scripts import prepare_data as prep
+from scripts import model_training as training
+
+data = loader.load()
+
+X_train, X_test, y_train, y_test = prep.prepare_data(data)
+
+model = training.train(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test)
+optimised_model = training.optimised_training(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test)
+optimised_model_rf = training.optimised_training_random_forest(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test)
